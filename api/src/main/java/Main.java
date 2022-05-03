3fd850ee32d34652fdfd08cc679e820b1c595a16
luckyhunterzzz <lucky_hunter@list.ru>
2022-05-04 01:05:41 +0500

    first commit

---
+++ api/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.UUID;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
